4c0e139d065d0bb3d2dbda70c1223876dbb9231b
azofeifa <jgazofeifa@gmail.com>
2016-07-27 11:58:38 -0600

    updates

--- CPP_src/test_run.sh
+++ CPP_src/test_run.sh
@@ -1,5 +1,14 @@
+#PBS -N gTFIv2
+#PBS -m ae
+#PBS -M joseph.azofeifa@colorado.edu
+#PBS -e /Users/azofeifa/correlation_networks/
+#PBS -o /Users/azofeifa/correlation_networks/
+#PBS -l walltime=72:00:00
+#PBS -l nodes=1:ppn=64
+#PBS -l mem=10gb
 
-./CORR join -bed_dir /Users/joazofeifa/Lab/new_motif_distances/EMG_out_files_human/ -o /Users/joazofeifa/Desktop/test.bed -fast 0 -window 10000 -distance 1000
+
+./CORR join -bed_dir /Users/joazofeifa/Lab/new_motif_distances/EMG_out_files_human/ -o /Users/joazofeifa/Desktop/test.bed -fast 0 -window 1000 -distance 1000
 
 
 #./CORR insert -bed /Users/joazofeifa/Lab/correlation_networks/files/joined_bidir.bed  -o /Users/joazofeifa/Desktop/coverage_stats.bed  -bg_dir /Users/joazofeifa/Lab/gro_seq_files/HCT116/bed_graph_files_2/
