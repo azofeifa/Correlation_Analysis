7935375f59821d4736191e41c440c6353fc05485
azofeifa <jgazofeifa@gmail.com>
2016-08-04 14:27:27 -0600

    updates

--- CPP_src/correlations.cpp
+++ CPP_src/correlations.cpp
@@ -139,7 +139,7 @@ void correlate::compute(vector<vector<double>> A, vector<double> counts, vector<
 				dist 	= -1;
 			}
 
-			if (P[i][j]>0.8){
+			if (P[i][j]>0.5){
 				line+=  to_string(IDS[i]) + ","+ to_string(IDS[j]) + "\t" + to_string(P[i][j]) + "," + to_string(dist) + "\n";
 			}
 		}
